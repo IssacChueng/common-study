b63fc130173b829aec8e7ff709087d334bbffa0e
siweizhang <siweizhang123>
2019-03-22 16:55:47 +0800

    common

---
+++ gamekyller/src/main/java/com/jeffrey/gamekyller/GamekyllerApplication.java
@@ -0,0 +1,13 @@
+package com.jeffrey.gamekyller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamekyllerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GamekyllerApplication.class, args);
+    }
+
+}
---
+++ gamekyller/src/test/java/com/jeffrey/gamekyller/GamekyllerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.jeffrey.gamekyller;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GamekyllerApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
